0e16c640ddc11ce7c24ef47a4d39c524b57bdd19
Ashish Negi <ashishnegi33@gmail.com>
2017-09-30 14:09:51 +0530

    Full screen mode and added readme.md

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# binReach
+
+Help people with disability reach any corner of window in minimum keyboard steps.
+
+## Idea :
+Divide the screen into smaller pieces with each user input to take her to the desired position.
+
+## Implementation :
+1. Use a transparent window.
+2. Create four quadrants in current selected step.
+3. Use left/right to go to next quadrant and up/down to go to next/previous step.
+4. Use enter to move out of binReach_mode.
+5. Long press another key to come back in binReach_mode.
--- test_wx.cpp
+++ test_wx.cpp
@@ -5,31 +5,23 @@
 class Simple : public wxFrame
 {
 public:
-    Simple(const wxString& title)
-		: wxFrame()
-	{
-		Create(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150));
-		Centre();
-	}
+  Simple(const wxString& title) : wxFrame()
+  {
+    Create(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150));
+  }
 };
 
 class MyApp : public wxApp
 {
 public:
-	bool OnInit()
-	{
-		Simple *simple = new Simple(wxT("Simple"));
-		simple->Show(true);
-		if (simple->SetTransparent(150))
-		{
-			std::cout << "Able to set transparent..\n";
-		}
-		else
-		{
-			std::cout << "Not able to set transparent..\n";
-		}
-		return true;
-	}
+  bool OnInit()
+  {
+    Simple *simple = new Simple(wxT("Simple"));
+    simple->Show(true);
+    assert(simple->SetTransparent(150));
+    assert(simple->ShowFullScreen(true, wxFULLSCREEN_ALL));
+    return true;
+  }
 };
 
 wxIMPLEMENT_APP(MyApp);
