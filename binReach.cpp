743c3c703ebf3d6433ddb34c0c097110f3f2dda8
Ashish Negi <ashishnegi33@gmail.com>
2017-10-01 00:21:19 +0530

    Using buttons again but removing panel.

--- binReach.cpp
+++ binReach.cpp
@@ -54,27 +54,22 @@ public:
     assert(reachIt->SetTransparent(150));
     assert(reachIt->ShowFullScreen(true, wxFULLSCREEN_ALL));
 
-    // ReachItGrid *grid =
-    new ReachItGrid(reachIt);
+    wxGridSizer *sizer = new wxGridSizer(2, 2, 0, 0);
 
+    wxButton *button1 = new wxButton(reachIt, wxID_ANY, wxString::FromAscii("1"));
+    sizer->Add(button1, wxSizerFlags().Expand());
 
-    // wxPanel *panel = new wxPanel(reachIt);
-    // wxGridSizer *sizer = new wxGridSizer(2, 2, 0, 0);
+    wxButton *button2 = new wxButton(reachIt, wxID_ANY, wxString::FromAscii("2"));
+    sizer->Add(button2, wxSizerFlags().Expand());
 
-    // wxButton *button1 = new wxButton(panel, wxID_ANY, wxString::FromAscii("1"));
-    // sizer->Add(button1, wxSizerFlags().Expand());
+    wxButton *button3 = new wxButton(reachIt, wxID_ANY, wxString::FromAscii("3"));
+    sizer->Add(button3, wxSizerFlags().Expand());
 
-    // wxButton *button2 = new wxButton(panel, wxID_ANY, wxString::FromAscii("2"));
-    // sizer->Add(button2, wxSizerFlags().Expand());
+    wxButton *button4 = new wxButton(reachIt, wxID_ANY, wxString::FromAscii("4"));
+    button4->MoveBeforeInTabOrder(button3);
+    sizer->Add(button4, wxSizerFlags().Expand());
 
-    // wxButton *button3 = new wxButton(panel, wxID_ANY, wxString::FromAscii("3"));
-    // sizer->Add(button3, wxSizerFlags().Expand());
-
-    // wxButton *button4 = new wxButton(panel, wxID_ANY, wxString::FromAscii("4"));
-    // button4->MoveBeforeInTabOrder(button3);
-    // sizer->Add(button4, wxSizerFlags().Expand());
-
-    // panel->SetSizerAndFit(sizer);
+    reachIt->SetSizerAndFit(sizer);
 
     return true;
   }
