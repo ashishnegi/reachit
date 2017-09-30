74b41e3293579ee78e13dffbc6b6b91e38d0f502
Ashish Negi <ashishnegi33@gmail.com>
2017-10-01 00:16:22 +0530

    Add a grid : use tab_wrap but that still does not expand so much.

--- Readme.md
+++ Readme.md
@@ -11,3 +11,4 @@ Divide the screen into smaller pieces with each user input to take her to the de
 3. Use left/right to go to next quadrant and up/down to go to next/previous step.
 4. Use enter to move out of binReach_mode.
 5. Long press another key to come back in binReach_mode.
+6. Show mouse cursor movement.
--- binReach.cpp
+++ binReach.cpp
@@ -1,10 +1,39 @@
 #include <wx/wx.h>
 #include <wx/grid.h>
 #include <wx/sizer.h>
-#include <wx/string.h>
 
 #include <iostream>
 #include <cassert>
+
+class ReachItGrid : public wxGrid
+{
+public:
+  ReachItGrid(wxWindow* parent) : wxGrid(parent, wxID_ANY)
+  {
+    CreateGrid(2,2);
+    SetTabBehaviour(Tab_Wrap);
+  }
+
+  virtual void OnSize(wxSizeEvent & event)
+  {
+    wxSize clientSize = GetClientSize();
+
+    long numCols = GetNumberCols();
+    long width = clientSize.GetWidth() / numCols;
+    for (long i = 0; i < numCols; ++i) {
+      SetColSize(0, width);
+    }
+
+    long numRows = GetNumberRows();
+    long height = clientSize.GetHeight() / numRows;
+    for (long j = 0; j < numRows; ++j) {
+      SetRowSize(0, height);
+    }
+
+    event.Skip();
+  }
+
+};
 
 class ReachItFrame : public wxFrame
 {
@@ -25,23 +54,27 @@ public:
     assert(reachIt->SetTransparent(150));
     assert(reachIt->ShowFullScreen(true, wxFULLSCREEN_ALL));
 
-    wxPanel *panel = new wxPanel(reachIt);
-    wxGridSizer *sizer = new wxGridSizer(2, 2, 0, 0);
+    // ReachItGrid *grid =
+    new ReachItGrid(reachIt);
 
-    wxButton *button1 = new wxButton(panel, wxID_ANY, wxString::FromAscii("1"));
-    sizer->Add(button1, wxSizerFlags().Expand());
 
-    wxButton *button2 = new wxButton(panel, wxID_ANY, wxString::FromAscii("2"));
-    sizer->Add(button2, wxSizerFlags().Expand());
+    // wxPanel *panel = new wxPanel(reachIt);
+    // wxGridSizer *sizer = new wxGridSizer(2, 2, 0, 0);
 
-    wxButton *button3 = new wxButton(panel, wxID_ANY, wxString::FromAscii("3"));
-    sizer->Add(button3, wxSizerFlags().Expand());
+    // wxButton *button1 = new wxButton(panel, wxID_ANY, wxString::FromAscii("1"));
+    // sizer->Add(button1, wxSizerFlags().Expand());
 
-    wxButton *button4 = new wxButton(panel, wxID_ANY, wxString::FromAscii("4"));
-    button4->MoveBeforeInTabOrder(button3);
-    sizer->Add(button4, wxSizerFlags().Expand());
+    // wxButton *button2 = new wxButton(panel, wxID_ANY, wxString::FromAscii("2"));
+    // sizer->Add(button2, wxSizerFlags().Expand());
 
-    panel->SetSizerAndFit(sizer);
+    // wxButton *button3 = new wxButton(panel, wxID_ANY, wxString::FromAscii("3"));
+    // sizer->Add(button3, wxSizerFlags().Expand());
+
+    // wxButton *button4 = new wxButton(panel, wxID_ANY, wxString::FromAscii("4"));
+    // button4->MoveBeforeInTabOrder(button3);
+    // sizer->Add(button4, wxSizerFlags().Expand());
+
+    // panel->SetSizerAndFit(sizer);
 
     return true;
   }
