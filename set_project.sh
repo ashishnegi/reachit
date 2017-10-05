e66da50a408566b245e21cfff0e4513a82940ef8
Ashish Negi <ashishnegi33@gmail.com>
2017-10-05 22:07:01 +0530

    Add linux release/debug reachit binaries.

--- set_project.sh
+++ set_project.sh
@@ -4,8 +4,10 @@
 # sudo apt-get install libgtk-3-dev build-essential checkinstall
 # tar -xvf wxWidgets-3.1.0.tar.bz2
 # mkdir wxWidgets-3.1.0/gtk_build
-# cd wxWidgets-3.1.0/gtk_build
+# cd wxWidgets-3.1.0/{gtk_build,gtk_build_release}
 # ../configure --disable-shared --enable-unicode --enable-debug --enable-debug_gdb
+# cd ../gtk_build_release
+# ../configure --disable-shared --enable-unicode --disable-debug
 # make -j8
 
 export PATH=$PATH:./wxWidgets-3.1.0/gtk_build
