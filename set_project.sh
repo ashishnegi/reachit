487f5446a46ec44212951ece53c803dbced2c160
Ashish Negi <ashishnegi33@gmail.com>
2017-10-05 22:16:03 +0530

    Make reachit release size smaller to 2.5M

--- set_project.sh
+++ set_project.sh
@@ -7,7 +7,9 @@
 # cd wxWidgets-3.1.0/{gtk_build,gtk_build_release}
 # ../configure --disable-shared --enable-unicode --enable-debug --enable-debug_gdb
 # cd ../gtk_build_release
+# export CXXFLAGS="-ffunction-sections -fdata-sections -Os"
+# export LDFLAGS="-Wl,--gc-sections"
 # ../configure --disable-shared --enable-unicode --disable-debug
 # make -j8
 
-export PATH=$PATH:./wxWidgets-3.1.0/gtk_build
+export PATH=$PATH:./wxWidgets-3.1.0/gtk_build_release
